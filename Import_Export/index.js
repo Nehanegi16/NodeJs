2aee03d5a55506fe4c6f26a768d7a29e37209ff4
Neha <139878899+Nehanegi16@users.noreply.github.com>
2023-08-19 15:16:22 +0530

    Add files via upload

---
+++ Import_Export/index.js
@@ -0,0 +1,6 @@
+const {add,sub,mul,div} =require('./oper.js')
+
+console.log(add(5,5))
+console.log(sub(40,10))
+console.log(mul(10,10))
+console.log(div(40,800))
---
+++ Import_Export/oper.js
@@ -0,0 +1,13 @@
+const add=(num1,num2)=>{
+    return num1+num2
+}
+const sub=(a,b)=>{
+    return a-b
+}
+const mul=(a,b)=>{
+    return a*b
+}
+const div=(a,b)=>{
+    return a/b
+}
+module.exports={add,sub,mul,div}
