e7f7c7adbedd27eea75a7fcd252a4d6e474549ab
Neha <139878899+Nehanegi16@users.noreply.github.com>
2023-08-19 15:03:25 +0530

    Update Path.js

--- Path_Module/Path.js
+++ Path_Module/Path.js
@@ -3,7 +3,7 @@ console.log(path.dirname('C:/Users/nitin/Nodejs Topic/Path_Module/Path.js'))
 console.log(path.extname('C:/Users/nitin/Nodejs Topic/Path_Module/Path.js'))
 console.log(path.parse('C:/Users/nitin/Nodejs Topic/Path_Module/Path.js'))  //if we want access all  paths
 
-const mypath=path.parse('C:/Users/nitin/Nodejs Topic/Path_Module/Path.js') /// if we want particular one field
+const mypath=path.parse('C:/Users/nitin/Nodejs Topic/Path_Module/Path.js') /// if we want particular one path
 console.log(mypath.base)
 console.log(mypath.root)
 console.log(mypath.name)
